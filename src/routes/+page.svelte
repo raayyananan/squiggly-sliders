323219116afdeda0093f11cdf6c793fbe47c9913
Rayyan Tariq <raytariq93@gmail.com>
2024-05-30 15:15:51 +0500

    color selection is functioning now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -152,7 +152,12 @@
         '--slider-color-7',
         '--slider-color-8',
     ]
-
+    const samplePassiveColors = [
+        '--slider-passive-color-1',
+        '--slider-passive-color-2',
+        '--slider-passive-color-3',
+        '--slider-passive-color-4',
+    ]
     let activePickerOpen = false;
     let passivePickerOpen = false;
 
@@ -185,7 +190,7 @@
             <div class="card-background pl-6 shadow shadow-black/15 rounded-2xl flex flex-col gap-4">
                 <div class="h-16 flex items-center justify-between">
                     <h2 class="font-extrabold">Squiggly Sliders</h2>
-                    <button class="global-theme-button button-shadow-left flex items-center w-36 justify-center h-full primary-gradient-background text-on-primary rounded-r-2xl group md:hover:brightness-95 active:brightness-90 md:active:brightness-90">
+                    <button class="global-theme-button button-shadow-left flex items-center w-36 justify-center h-full primary-gradient-background text-on-primary rounded-r-2xl group md:active:brightness-95">
                         <div class="flex items-center gap-2 group-active:scale-[0.95] transition-transform duration-75">
                             <div class="text-sm font-bold">About</div>
                             <div class="text-lg material-symbols-rounded bold -translate-y-px origin-bottom-right">keyboard_arrow_down</div>
@@ -211,31 +216,28 @@
                 </div>
 
                 <div class="flex h-12 button-shadow-default rounded-2xl">
-                    <button on:click={() => {activePickerOpen = !activePickerOpen; console.log(activePickerOpen);}} 
-                        class="{activePickerOpen ? 'cursor-auto' : 'cursor-pointer'} flex-1 dark:flex-[4] h-full text-on-primary rounded-l-xl flex items-center justify-center font-bold text-sm group">
-                        <!-- style="background: linear-gradient(180deg, {activeColor[0] === '-' ? `var(${activeColor})` : activeColor} var(--p1), var(--c2) var(--p2), {activeColor[0] === '-' ? `var(${activeColor})` : activeColor} var(--p3)" -->
+                    <button on:click={() => {activePickerOpen = !activePickerOpen;}} 
+                        class="{activePickerOpen ? 'cursor-auto' : 'cursor-pointer'} eyedrop-button flex-1 dark:flex-[4] h-full text-on-primary rounded-l-xl flex items-center justify-center font-bold text-sm group">
                         
                         {#if activePickerOpen} 
                             <div transition:fade={{duration: 100}} class="fixed top-0 left-0 w-full h-full z-10 backdrop-blur-sm bg-black/0 dark:bg-white/[2.5%]"></div>
                         {/if}
-
-                        <div class="relative duration-300 primary-gradient-background text-on-primary dark:text-on-surface flex-shrink-0 flex flex-col items-center justify-center
+                        
+                        <div class="relative duration-300 text-on-primary dark:text-on-surface flex-shrink-0 flex flex-col items-center justify-center
                            {activePickerOpen ? 
-                                'w-[calc(100%_+_80px)] bg-inverse-surface text-inverse-on-surface h-40 rounded-2xl z-10 shadow-xl rounded-l-2xl easing-bounce-out-light' : 
-                                'w-full h-full rounded-l-2xl easing-emphasized bg-primary'}"
-                            style="{activePickerOpen ? 
-                                        '--primary-gradient-background-primary: var(--md-sys-color-inverse-surface); --c2: var(--md-sys-color-inverse-surface)' : 
-                                        `--primary-gradient-background-primary: ${activeColor[0] === '-' ? `var(${activeColor})` : activeColor}; --c2: ${activeColor[0] === '-' ? `var(${activeColor}-gradient-stop)` : activeColor};`
-                                    }">
-                                        <!-- `background: linear-gradient(180deg, {activeColor[0] === '-' ? `var(${activeColor})` : activeColor} var(--p1), var(--c2) var(--p2), {activeColor[0] === '-' ? `var(${activeColor})` : activeColor} var(--p3)` -->
+                                'w-[calc(100%_+_80px)] translate-x-3 translate-y-4 bg-inverse-surface text-inverse-on-surface h-40 rounded-2xl z-10 shadow-xl rounded-l-2xl easing-bounce-out-light' : 
+                                'w-full h-full rounded-l-2xl easing-emphasized z-0'}"
+                            style="{activePickerOpen ? '' : `background-color: var(${activeColor})`}">
+
+                        <div class="absolute top-0 left-0 w-full h-full bg-light-gradient rounded-l-xl {activePickerOpen && 'opacity-0'}"></div>
                             
-                            <div class="eyedrop-button rounded-l-2xl h-12 flex-shrink flex justify-between w-full items-center px-3 transition duration-300 easing-emphasized">
+                            <div class="rounded-l-2xl h-12 flex-shrink flex justify-between w-full items-center px-3 transition duration-300 easing-emphasized">
                                 <div aria-hidden="true" class="opacity-0 increment-button rounded-full h-8 w-8 text-on-surface flex items-center justify-center flex-shrink-0"></div>
                                 <div class="flex items-center justify-center gap-1 5">
                                     <div class="text-base origin-bottom-left material-symbols-rounded bold -translate-y-[0.5px]">colorize</div>
                                     <div class="text-sm font-bold">Active</div>
                                 </div>
-                                <button on:click={() => {}} class="{activePickerOpen ? 'opacity-1 delay-100 duration-200' : 'opacity-0 duration-100'} increment-button rounded-full flex items-center justify-center md:hover:brightness-95 active:brightness-95 md:active:brightness-[.93] active:scale-95 transition-all duration-100 flex-shrink-0">
+                                <button tabindex={activePickerOpen ? 0 : -1} on:click={() => {}} class="{activePickerOpen ? 'opacity-1 delay-100 duration-200' : 'opacity-0 duration-100'} increment-button rounded-full flex items-center justify-center md:hover:brightness-95 active:brightness-95 md:active:brightness-[.93] active:scale-95 transition-all duration-100 flex-shrink-0">
                                     <div class="material-symbols-rounded text-lg font-semibold text-inverse-on-surface">close</div>
                                 </button>
                             </div>
@@ -243,7 +245,7 @@
                             <div class="w-full flex-shrink-0 {activePickerOpen ? 'flex-1 h-28' : 'flex-0 h-0'} overflow-hidden transition-all duration-300 easing-emphasized">
                                 <div class="{activePickerOpen ? 'opacity-1 delay-100 duration-200' : 'opacity-0 scale-90 duration-75'} w-full h-full grid grid-cols-4 gap-2 p-3 pt-0 place-items-center">
                                     {#each sampleColors as color}
-                                    <button on:click={() => {setProperties(color)}} class="h-full w-full rounded-xl border border-b-2 border-white/15 relative group/button" style="background-color: {color[0] === '-' ? `var(${color})` : `${color}`}">
+                                    <button on:click={() => {setProperties(color)}} tabindex={activePickerOpen ? 0 : -1} class="h-full w-full rounded-xl border border-b-2 border-white/15 relative group/button" style="background-color: {color[0] === '-' ? `var(${color})` : `${color}`}">
                                         <div class="absolute top-0 left-0 w-full h-full rounded-xl bg-gradient-to-br from-transparent to-transparent via-white/15 opacity-50 group-hover/button:opacity-100 transition duration-150"></div>
                                     </button>
                                     {/each}
@@ -253,14 +255,46 @@
                         </div>
 
                     </button>
-                    <a class="flex-1 dark:flex-[3] h-full on-surface-gradient-background text-surface rounded-r-xl flex items-center justify-center font-bold text-sm md:hover:brightness-95 active:brightness-90 md:active:brightness-90 group">
-                        <div class="w-full h-full flex items-center justify-center">
-                            <button class="eyedrop-button rounded-r-2xl h-full flex-auto flex justify-center items-center gap-1.5 active:scale-[0.975] transition-transform duration-150">
-                                <div class="text-base origin-bottom-left material-symbols-rounded bold -translate-y-[0.5px]">colorize</div>
-                                <div class="text-sm font-bold">Passive</div>
-                            </button>
+                    
+                    <button on:click={() => {passivePickerOpen = !passivePickerOpen;}} 
+                        class="{passivePickerOpen ? 'cursor-auto' : 'cursor-pointer'} eyedrop-button flex-1 dark:flex-[4] h-full text-on-primary rounded-r-xl flex items-center justify-center font-bold text-sm group">
+                        
+                        {#if passivePickerOpen} 
+                            <div transition:fade={{duration: 100}} class="fixed top-0 left-0 w-full h-full z-10 backdrop-blur-sm bg-black/0 dark:bg-white/[2.5%]"></div>
+                        {/if}
+                        
+                        <div class="relative duration-300 text-on-primary dark:text-on-surface flex-shrink-0 flex flex-col items-center justify-center
+                           {passivePickerOpen ? 
+                                'w-[calc(100%_+_80px)] -translate-x-3 translate-y-4 bg-inverse-surface text-inverse-on-surface h-40 rounded-2xl z-10 shadow-xl rounded-r-2xl easing-bounce-out-light' : 
+                                'w-full h-full rounded-r-2xl easing-emphasized'}"
+                            style="{passivePickerOpen ? '' : `background-color: var(${passiveColor}); color: var(${passiveColor}-text);`}">
+
+                        <div class="absolute top-0 left-0 w-full h-full bg-light-gradient rounded-l-xl {passivePickerOpen && 'opacity-0'}"></div>
+                            
+                            <div class="rounded-l-2xl h-12 flex-shrink flex justify-between w-full items-center px-3 transition duration-300 easing-emphasized">
+                                <div aria-hidden="true" class="opacity-0 increment-button rounded-full h-8 w-8 text-on-surface flex items-center justify-center flex-shrink-0"></div>
+                                <div class="flex items-center justify-center gap-1 5">
+                                    <div class="text-base origin-bottom-left material-symbols-rounded bold -translate-y-[0.5px]">colorize</div>
+                                    <div class="text-sm font-bold">Passive</div>
+                                </div>
+                                <button tabindex={passivePickerOpen ? 0 : -1} on:click={() => {}} class="{passivePickerOpen ? 'opacity-1 delay-100 duration-200' : 'opacity-0 duration-100'} increment-button rounded-full flex items-center justify-center md:hover:brightness-95 active:brightness-95 md:active:brightness-[.93] active:scale-95 transition-all duration-100 flex-shrink-0">
+                                    <div class="material-symbols-rounded text-lg font-semibold text-inverse-on-surface">close</div>
+                                </button>
+                            </div>
+
+                            <div class="w-full flex-shrink-0 {passivePickerOpen ? 'flex-1 h-28' : 'flex-0 h-0'} overflow-hidden transition-all duration-300 easing-emphasized">
+                                <div class="{passivePickerOpen ? 'opacity-1 delay-100 duration-200' : 'opacity-0 scale-90 duration-75'} w-full h-full grid grid-cols-2 gap-2 p-3 pt-0 place-items-center">
+                                    {#each samplePassiveColors as color}
+                                    <button on:click={() => {setProperties(activeColor, color)}} tabindex={passivePickerOpen ? 0 : -1} class="h-full w-full rounded-xl border border-b-2 border-white/15 relative group/button" style="background-color: {color[0] === '-' ? `var(${color})` : `${color}`}">
+                                        <div class="absolute top-0 left-0 w-full h-full rounded-xl bg-gradient-to-br from-transparent to-transparent via-white/15 opacity-50 group-hover/button:opacity-100 transition duration-150"></div>
+                                    </button>
+                                    {/each}
+                                </div>
+                            </div>
+
                         </div>
-                    </a>
+
+                    </button>
                 </div>
 
                 <div class="flex flex-col gap-4 mt-3">
@@ -340,19 +374,19 @@
                     </div>
 
                     <div class="w-full h-10 flex rounded-xl border border-outline-variant my-2">
-                        <button on:click={() => {setProperties('--slider-color-1', passiveColor, 7, 0, 0.6, 1, 4)}} 
+                        <button on:click={() => {setProperties('--slider-color-1', '--slider-passive-color-2', 7, 0, 0.6, 1, 4)}} 
                             class="flex-1 flex items-center justify-center border-r border-outline-variant rounded-l-xl font-semibold hover:bg-surface-container-highest active:brightness-[.98]">
                             <img class="saturate-0 dark:invert h-5" src="/wave3.svg" />
                         </button>
-                        <button on:click={() => {setProperties('--slider-color-4', passiveColor, 4, 0, 1.5, passiveWavelength, 1)}} 
+                        <button on:click={() => {setProperties('--slider-color-4', '--slider-passive-color-2', 4, 0, 1.5, passiveWavelength, 1)}} 
                             class="flex-1 flex items-center justify-center border-r border-outline-variant hover:bg-surface-container-highest active:brightness-[.98]">
                             <img class="saturate-0 dark:invert h-[.31rem]" src="/wave4.svg" />
                         </button>
-                        <button on:click={() => {setProperties('--md-sys-color-tertiary', passiveColor, 4, 2, 1, 1, 4)}} 
+                        <button on:click={() => {setProperties('--md-sys-color-tertiary', '--slider-passive-color-2', 4, 2, 1, 1, 4)}} 
                             class="flex-1 flex items-center justify-center border-r border-outline-variant hover:bg-surface-container-highest active:brightness-[.98]">
                             <img class="saturate-0 dark:invert h-4" src="/wave1.svg" />
                         </button>
-                        <button on:click={() => {setProperties('--slider-color-5', passiveColor, 4, 0, 0.1, 2, 1)}} 
+                        <button on:click={() => {setProperties('--slider-color-5', '--slider-passive-color-2', 4, 0, 0.1, 2, 1)}} 
                             class="flex-1 flex items-center justify-center rounded-r-xl hover:bg-surface-container-highest active:brightness-[.98]">
                             <svg class="saturate-0 dark:invert h-3 w-3 rotate-90" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 0c53 0 96 43 96 96v3.6c0 15.7-12.7 28.4-28.4 28.4H188.4c-15.7 0-28.4-12.7-28.4-28.4V96c0-53 43-96 96-96zM41.4 105.4c12.5-12.5 32.8-12.5 45.3 0l64 64c.7 .7 1.3 1.4 1.9 2.1c14.2-7.3 30.4-11.4 47.5-11.4H312c17.1 0 33.2 4.1 47.5 11.4c.6-.7 1.2-1.4 1.9-2.1l64-64c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3l-64 64c-.7 .7-1.4 1.3-2.1 1.9c6.2 12 10.1 25.3 11.1 39.5H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H416c0 24.6-5.5 47.8-15.4 68.6c2.2 1.3 4.2 2.9 6 4.8l64 64c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0l-63.1-63.1c-24.5 21.8-55.8 36.2-90.3 39.6V240c0-8.8-7.2-16-16-16s-16 7.2-16 16V479.2c-34.5-3.4-65.8-17.8-90.3-39.6L86.6 502.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l64-64c1.9-1.9 3.9-3.4 6-4.8C101.5 367.8 96 344.6 96 320H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H96.3c1.1-14.1 5-27.5 11.1-39.5c-.7-.6-1.4-1.2-2.1-1.9l-64-64c-12.5-12.5-12.5-32.8 0-45.3z"/></svg>
                         </button>
@@ -365,7 +399,13 @@
                     <h2 class="font-extrabold">Preview</h2>
                 </div>
 
-                <div class="flex-auto flex items-center justify-center">
+                <div class="flex-auto flex flex-col items-center justify-center relative">
+                    {#if passiveColor === '--slider-passive-color-1' || passiveColor == '--slider-passive-color-3'}
+                    <div class="absolute -top-4 left-0 text-xs text-outline flex gap-2  text-balance">
+                        <div class="material-symbols-rounded text-base">info</div>
+                        <p>Lighter passive colors cannot be seen properly in the slider preview. Please make sure your sliders have a sufficient contrast ratio. Add slider to view proper result.</p>
+                    </div>
+                    {/if}
                     <div class="w-80">
                         <SquigglySlider value={6} active={activeColor} passive={passiveColor} activeAmplitude={activeAmplitude} passiveAmplitude={passiveAmplitude} activeWavelength={activeWavelength} passiveWavelength={passiveWavelength} speedFactor={speedFactor} />
                     </div>
@@ -387,7 +427,7 @@
                     focus-visible:scale-[1.02] active:scale-[0.97] primary-gradient-background transition-transform active:duration-[35ms] duration-[350ms] easing-bounce-out">
                         <div class="flex gap-1.5 items-center">
                             <div class="text-lg material-symbols-rounded bold -translate-y-px">add</div>
-                            <div class="text-sm font-bold">Add Custom</div>
+                            <div class="text-sm font-bold">Add Slider</div>
                         </div>
                     </button>
                 </div>
@@ -399,14 +439,22 @@
 
 
 <style lang="scss">
-    @media (prefers-color-scheme: dark) {
-        .bg-inverse-surface {
-            transition-duration: --primary-gradient-background-primary 300ms, --c2 300ms;
-            --md-sys-color-inverse-surface: var(--md-sys-color-surface-container-high);
-            // background: var(--md-sys-color-surface-container-high);
-        }
+    .transition-all.duration-300.easing-emphasized {
+        transition: all 300ms var(--md-sys-motion-easing-emphasized), z-index 300ms step-end;
     }
 
+    .eyedrop-button {
+        .material-symbols-rounded {
+            animation: drop-back 500ms cubic-bezier(0.2, -0.55, 0.265, 2);
+        }
+        .text-sm {
+            animation: pushed-back 500ms cubic-bezier(0.2, -0.55, 0.265, 1);
+        }
+
+        &:active, &:focus-visible {
+            .material-symbols-rounded, .text-sm {animation: none;}
+        }
+    }
 
     .add-default-button {
         .material-symbols-rounded {
@@ -433,19 +481,6 @@
             .material-symbols-rounded, .text-sm {animation: none;}
         }
     }
-
-    // .eyedrop-button {
-        // .material-symbols-rounded {
-        //     animation: drop-back 500ms cubic-bezier(0.2, -0.55, 0.265, 2);
-        // }
-        // .text-sm {
-        //     animation: pushed-back 500ms cubic-bezier(0.2, -0.55, 0.265, 1);
-        // }
-
-        // &:active, &:focus-visible {
-        //     .material-symbols-rounded, .text-sm {animation: none;}
-        // }
-    // }
 
     .reset-button {
         .material-symbols-rounded {
@@ -480,7 +515,7 @@
     }
     @keyframes pushed-back {
         0% {translate: 0px 0}
-        50% {translate: -3px 0;}
+        50% {translate: -1px 0;}
         100% {translate: 0 0}
     }
 
